5ca7d77b872b8d01a6ab8e87882aeb66dcc9792d
GoGei <rich.290401@gmail.com>
2022-04-27 14:09:35 +0300

    Added url compatibility Added post read more functionality

--- Blog/urls.py
+++ Blog/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     url(r'^', include('urls')),
-    url(r'', include('Blog.Home.urls')),
     url(r'', include('Blog.Login.urls')),
     url(r'^account/', include('Blog.Account.urls')),
+    url(r'', include('Blog.Home.urls')),
 ]
